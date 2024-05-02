3d6f291db46b49640eea682697b2f5b5672b5a6d
Your Name <alexissanmiguel03@gmail.com>
2024-05-02 12:40:50 -0500

    Update token types in Home.tsx and types.d.ts

--- src/Components/TokenDisplay.tsx
+++ src/Components/TokenDisplay.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
 import { Box, Text } from '@chakra-ui/react';
 
-const TokenDisplay = ({ tokens }) => {
+interface TokenDisplayProps {
+  tokens: Token[];
+}
+
+const TokenDisplay = ({ tokens }: TokenDisplayProps) => {
   return (
-    <Box
-      h={64}
-      borderWidth={1}
-      borderRadius={8}
-      overflow={'auto'}
-      px={4}
-      p={2}
-      mt={6}
-    >
+    <Box h={64} borderWidth={1} borderRadius={8} overflow={'auto'} px={4} p={2} mt={6}>
       {tokens.map((token, index) => (
         <Text key={index}>{`(${token[0]}, '${token[1]}')`}</Text>
       ))}
