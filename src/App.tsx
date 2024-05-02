d8fe17b27f914031722aba25896b263771e62547
Your Name <alexissanmiguel03@gmail.com>
2024-05-02 11:33:53 -0500

    Update logo size and heading size in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     <AnimatePresence mode='wait'>
       <Container maxW={'7xl'} mt={2}>
         <Flex alignItems={'center'} textAlign={'center'} justifyContent={'center'}>
-          <Image src='logo.png' alt='Logo' boxSize={36} />
-          <Heading as={'h1'} size={'3xl'} textAlign={'center'} color={'blue.400'}>
+          <Image src='logo.png' alt='Logo' boxSize={44} />
+          <Heading as={'h1'} size={'4xl'} textAlign={'center'} color={'blue.400'}>
             <Link to={'/'}>NOVA</Link>
           </Heading>
         </Flex>
