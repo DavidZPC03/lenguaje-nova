fc3fd1e6a3b3e13a6473918f7f6016f9862dc3d6
DavidZPC03 <davidzpc03@gmail.com>
2025-02-26 13:35:00 -0600

    "Code of refactor of upload tokens"

--- src/Components/Tokens.tsx
+++ src/Components/Tokens.tsx
@@ -1,6 +1,8 @@
-import { Box, Button, Flex, Heading } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, IconButton} from '@chakra-ui/react';
+import { FiDownload, FiUpload } from 'react-icons/fi';
 import { javascript } from '@codemirror/lang-javascript';
 import ReactCodeMirror from '@uiw/react-codemirror';
+
 
 interface TokenDisplayProps {
   tokens: {
@@ -15,13 +17,34 @@ interface TokenDisplayProps {
   }[];
 }
 
-export function Tokens({ tokens, errores }: TokenDisplayProps) {
+
+export function Tokens({ tokens, errores, handleCodeChange }: TokenDisplayProps) {
   const lines = tokens.reduce((acc, token) => {
     if (!acc[token.line]) acc[token.line] = [];
     acc[token.line].push(token.type);
     return acc;
   }, {});
 
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target?.result;
+        handleCodeChange(text?.toString() || '');
+      };
+      reader.readAsText(file);
+    } else {
+      toast({
+        title: 'Error',
+        description: 'No file was selected.',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
   const downloadTokens = () => {
     if (errores.length > 0) {
       alert('No puedes descargar los tokens debido a errores en el código.');
@@ -47,14 +70,33 @@ export function Tokens({ tokens, errores }: TokenDisplayProps) {
 
   return (
     <>
-      <Box flex={0.5}>
+      <Box flex={0.5} >
         <Flex justifyContent={'space-between'} alignItems={'center'}>
           <Heading fontSize={'2xl'} fontWeight={'semibold'}>
             Tokens
           </Heading>
+          <Box flex={0.5} textAlign={'right'}> 
           <Button onClick={downloadTokens} colorScheme='blue' isDisabled={errores.length > 0}>
             Descargar tokens
           </Button>
+          <input
+              type='file'
+              id='fileInput'
+              style={{ display: 'none' }}
+              onChange={handleFileUpload}
+              accept='.nova'
+            />
+            <label htmlFor='fileInput'>
+              <IconButton
+                as='span'
+                colorScheme='gray'
+                aria-label='Cargar archivo'
+                icon={<FiUpload />}
+                mr={2}
+              />
+            </label>
+          </Box>
+
         </Flex>
         <Box
           mt={2}
@@ -81,3 +123,7 @@ export function Tokens({ tokens, errores }: TokenDisplayProps) {
     </>
   );
 }
+function toast(arg0: { title: string; description: string; status: string; duration: number; isClosable: boolean; }) {
+  throw new Error('Function not implemented.');
+}
+
